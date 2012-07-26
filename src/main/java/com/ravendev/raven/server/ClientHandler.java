24199be5fe63c2f6f35f87522a8989f0d7ddd29a
Buddy251 <buddy251@googlemail.com>
2012-07-26 16:21:28 +0200

    Fixed Network Starting Error!

--- src/main/java/com/ravendev/raven/server/ClientHandler.java
+++ src/main/java/com/ravendev/raven/server/ClientHandler.java
@@ -1,5 +1,8 @@
 package com.ravendev.raven.server;
 
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
@@ -8,7 +11,34 @@ public class ClientHandler extends SimpleChannelHandler {
 
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent event) throws Exception {
-		Object message = event.getMessage();
-		//TODO packet received, handle!
+		Channel client = event.getChannel();
+		ChannelBuffer readBuffer = (ChannelBuffer) event.getMessage();
+		int director = readBuffer.readInt();
+		byte[] bytearr = new byte[readBuffer.readableBytes()];
+		readBuffer.readBytes(bytearr);
+//		String out = new String(bytearr);
+//		System.out.println("Incomming: " + out);
+		switch(director){
+		case 1:
+			ChannelBuffer wbuff = ChannelBuffers.buffer(256);
+			wbuff.writeInt(1);
+			String message="OK";
+			wbuff.writeShort((short)message.length());
+			for(int i=0;i<message.length(); i++){
+				wbuff.writeByte((byte)message.charAt(i));
+			}
+			client.write(wbuff);
+			wbuff.clear();
+			wbuff.writeInt(2);
+			wbuff.writeFloat(0);
+			wbuff.writeFloat(0);
+			wbuff.writeFloat(0);
+			for(int i = 0; i<10; i++){
+				wbuff.writeShort(i+1);
+				wbuff.writeShort(i+1);
+			}
+			client.write(wbuff);
+			break;
+		}
 	}
 }
--- src/main/java/com/ravendev/raven/server/RavenServer.java
+++ src/main/java/com/ravendev/raven/server/RavenServer.java
@@ -1,21 +1,42 @@
 package com.ravendev.raven.server;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelFactory;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
 public class RavenServer {
 
 	public RavenServer() {
-		ServerBootstrap bootstrap = new ServerBootstrap();
-		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+//		ServerBootstrap bootstrap = new ServerBootstrap();
+//		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+//			public ChannelPipeline getPipeline() throws Exception {
+//				ChannelPipeline pipeline = Channels.pipeline();
+//				pipeline.addLast("handler", new ClientHandler());
+//				return pipeline;
+//			}
+//		});public void run(){
+		ChannelFactory factory =
+	            new NioServerSocketChannelFactory(
+	                    Executors.newCachedThreadPool(),
+	                    Executors.newCachedThreadPool());
 
-			public ChannelPipeline getPipeline() throws Exception {
-				ChannelPipeline pipeline = Channels.pipeline();
-				pipeline.addLast("handler", new ClientHandler());
-				return pipeline;
-			}
-		});
-	}
+	        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+
+	        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+	            public ChannelPipeline getPipeline() {
+	                return Channels.pipeline(new ClientHandler());
+	            }
+	        });
+
+	        bootstrap.setOption("child.tcpNoDelay", true);
+	        bootstrap.setOption("child.keepAlive", true);
+
+	        bootstrap.bind(new InetSocketAddress(4242));
+		}
 }
