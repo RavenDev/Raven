937d601a5e15d309e9af87aa6f925e9c76d1bc85
Buddy251 <buddy251@googlemail.com>
2012-07-26 16:32:49 +0200

    Added Support for some Packets!

--- src/main/java/com/ravendev/raven/server/ClientHandler.java
+++ src/main/java/com/ravendev/raven/server/ClientHandler.java
@@ -1,5 +1,7 @@
 package com.ravendev.raven.server;
 
+import java.awt.Color;
+
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
@@ -16,29 +18,39 @@ public class ClientHandler extends SimpleChannelHandler {
 		int director = readBuffer.readInt();
 		byte[] bytearr = new byte[readBuffer.readableBytes()];
 		readBuffer.readBytes(bytearr);
-//		String out = new String(bytearr);
-//		System.out.println("Incomming: " + out);
-		switch(director){
-		case 1:
-			ChannelBuffer wbuff = ChannelBuffers.buffer(256);
-			wbuff.writeInt(1);
-			String message="OK";
-			wbuff.writeShort((short)message.length());
-			for(int i=0;i<message.length(); i++){
-				wbuff.writeByte((byte)message.charAt(i));
+			if(director ==  1) { //Login
+				ChannelBuffer wbuff = ChannelBuffers.buffer(256);
+				wbuff.writeInt(1);
+				String message="OK";
+				wbuff.writeShort((short)message.length());
+				for(int i=0;i<message.length(); i++){
+					wbuff.writeByte((byte)message.charAt(i));
+				}
+				client.write(wbuff);
+				wbuff.clear();
+				wbuff.writeInt(2);
+				wbuff.writeFloat(0);
+				wbuff.writeFloat(0);
+				wbuff.writeFloat(0);
+				for(int i = 0; i<10; i++){
+					wbuff.writeShort(i+1);
+					wbuff.writeShort(i+1);
+				}
+				client.write(wbuff);
+			} else if(director == 3) { //Get Chunk
+				;
+			} else if(director == 19) { //PING
+				ChannelBuffer wbuff = ChannelBuffers.buffer(256);
+				wbuff.writeInt(19);
+				wbuff.writeInt(Color.red.getRGB());
+				String message="This Server is running Raven!";
+				wbuff.writeShort((short)message.length());
+				for(int i=0;i<message.length(); i++){
+					wbuff.writeByte((byte)message.charAt(i));
+				}
+				client.write(wbuff);
+				System.out.println("[" + client.getRemoteAddress().toString() + "] pinged!");
 			}
-			client.write(wbuff);
-			wbuff.clear();
-			wbuff.writeInt(2);
-			wbuff.writeFloat(0);
-			wbuff.writeFloat(0);
-			wbuff.writeFloat(0);
-			for(int i = 0; i<10; i++){
-				wbuff.writeShort(i+1);
-				wbuff.writeShort(i+1);
-			}
-			client.write(wbuff);
-			break;
-		}
+		System.out.println(director);
 	}
 }
